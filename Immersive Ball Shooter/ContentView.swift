31e050770a22f54220ebefb9732a11b9321ec7ca
HenryLam <139121114+HenryLamBlog@users.noreply.github.com>
2024-02-29 20:54:27 -0600

    Initial commit

--- Immersive Ball Shooter/ContentView.swift
+++ Immersive Ball Shooter/ContentView.swift
@@ -4,52 +4,249 @@
 //
 //  Created by Henry Lam on 29/2/2024.
 //
-
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
-
+    // MARK: - State Properties
+    
+    @State private var timeRemaining = 30
+    @State private var timeCache = 30
+    @State private var readyTime = 3
+    @State private var gameStart = false
+    @State private var loseGame = false
+    @State private var winGame = false
+    
+    // Timer to update countdown
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    // State to manage immersive space visibility
     @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+    
+    // Environment object to access shagreen data
+    @EnvironmentObject var counter: CounterModel
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
 
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
+        NavigationStack {
+            VStack {
+                // Display appropriate views based on game state
+                if loseGame {
+                    gameOverView()
+                } else if winGame {
+                    winGameView()
+                } else {
+                    if !gameStart {
+                        gameSetupView()
+                    } else if !showImmersiveSpace {
+                        countdownView()
+                    }
+                    if gameStart && showImmersiveSpace {
+                        gamePlayingView()
+                    }
+                }
+            }
         }
-        .padding()
+        .frame(width: 1100, height: 500)
         .onChange(of: showImmersiveSpace) { _, newValue in
+            // Handle changes in immersive space visibility
             Task {
                 if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
+                    await openImmersiveSpace(id: "ImmersiveSpace")
+                } else {
                     await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
                 }
+            }
+        }
+    }
+
+    // MARK: - Game Views
+    
+    private func gameOverView() -> some View {
+        // View displayed when the game is lost
+        VStack {
+            Text("You Lost!")
+                .font(.system(size: 45))
+                .fontWeight(.bold)
+            Spacer(minLength: 20)
+            Text("You shot \(counter.ballCounter)/\(counter.ballMax) green balls in \(timeCache)s.")
+                .font(.system(size: 64))
+                .fontWeight(.bold)
+            Spacer(minLength: 20)
+            Button("Try Again!") {
+                resetGame()
+            }
+            .toggleStyle(.button)
+            .font(.system(size: 30))
+            .fontWeight(.bold)
+            .frame(width: 360, height: 120)
+        }
+    }
+    
+    private func winGameView() -> some View {
+        // View displayed when the game is won
+        VStack {
+            Text("You Win!")
+                .font(.system(size: 45))
+                .fontWeight(.bold)
+            Spacer(minLength: 20)
+            Text("Congratulations!")
+                .font(.system(size: 64))
+                .fontWeight(.bold)
+             Text("You shot all \(counter.ballCounter) green balls in \(timeCache - timeRemaining)s!")
+                .font(.system(size: 64))
+                .fontWeight(.bold)
+            Spacer(minLength: 20)
+            Button("Play Again!") {
+                resetGame()
+            }
+            .toggleStyle(.button)
+            .font(.system(size: 30))
+            .fontWeight(.bold)
+            .frame(width: 360, height: 120)
+        }
+    }
+    
+    private func gameSetupView() -> some View {
+        // View for setting up the game
+        VStack {
+            Text("Shoot \(counter.ballMax) green balls in \(timeCache)s!")
+                .font(.system(size: 54))
+                .fontWeight(.bold)
+            settingsView(title: "Time Setting", buttons: timeSettingButtons())
+            settingsView(title: "Green Ball Setting", buttons: greenBallSettingButtons())
+            Spacer(minLength: 20)
+            startGameButton()
+        }
+    }
+    
+    private func settingsView(title: String, buttons: some View) -> some View {
+        // View for displaying settings
+        VStack {
+            Text(title)
+                .font(.system(size: 28))
+                .padding(.top, 20)
+                .padding(.bottom, 10)
+            buttons
+        }
+    }
+
+    private func timeSettingButtons() -> some View {
+        // Buttons for setting time
+        HStack {
+            Button("15s") { timeCache = 15 }
+            Button("30s") { timeCache = 30 }
+            Button("60s") { timeCache = 60 }
+        }
+    }
+
+    private func greenBallSettingButtons() -> some View {
+        // Buttons for setting green ball count
+        HStack {
+            Button("10") { counter.ballMax = 10 }
+            Button("25") { counter.ballMax = 25 }
+            Button("50") { counter.ballMax = 50 }
+        }
+    }
+
+    private func startGameButton() -> some View {
+        // Button to start the game
+        Button("Play") {
+            gameStart = true
+            timeRemaining = timeCache
+        }
+        .toggleStyle(.button)
+        .font(.system(size: 30))
+        .fontWeight(.bold)
+        .frame(width: 360, height: 120)
+    }
+
+    private func countdownView() -> some View {
+        // View for the countdown before the game starts
+        Text("\(readyTime)")
+            .font(.system(size: 160))
+            .fontWeight(.bold)
+            .onReceive(timer) { _ in
+                if readyTime > 1 {
+                    readyTime -= 1
+                } else {
+                    showImmersiveSpace = true
+                    timer.upstream.connect().cancel()
+                }
+            }
+    }
+
+    private func gamePlayingView() -> some View {
+        // View for playing the game
+        VStack {
+            Text("\(timeRemaining)")
+                .font(.system(size: 142))
+                .fontWeight(.bold)
+                .onReceive(timer) { _ in
+                    if timeRemaining > 0 {
+                        timeRemaining -= 1
+                    } else {
+                        gameEnd()
+                    }
+                }
+            Text("\(counter.ballCounter)/\(counter.ballMax) green balls shot!!")
+                .font(.system(size: 52))
+                .fontWeight(.semibold)
+            Button("Stop") { gameEnd() }
+                .toggleStyle(.button)
+                .font(.system(size: 30))
+                .fontWeight(.bold)
+                .frame(width: 360, height: 120)
+        }
+        .onReceive(timer) { _ in
+            if counter.ballCounter == counter.ballMax {
+                gameWin()
+            }
+        }
+    }
+    
+    // MARK: - Game Logic
+
+    private func resetGame() {
+        // Reset game parameters
+        loseGame = false
+        winGame = false
+        readyTime = 3
+        counter.ballCounter = 0
+        gameStart = false
+        timeRemaining = timeCache
+    }
+
+    private func gameWin() {
+        // Handle game win scenario
+        gameStart = false
+        showImmersiveSpace = false
+        winGame = true
+        timer.upstream.connect().cancel()
+    }
+    
+    private func gameEnd() {
+        // Handle game end scenario
+        gameStart = false
+        showImmersiveSpace = false
+        loseGame = true
+        timer.upstream.connect().cancel()
+    }
+
+    private func toggleImmersiveSpace() {
+        // Toggle immersive space visibility
+        Task {
+            if showImmersiveSpace {
+                await openImmersiveSpace(id: "ImmersiveSpace")
+            } else {
+                await dismissImmersiveSpace()
             }
         }
     }
 }
 
-#Preview(windowStyle: .automatic) {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- Immersive Ball Shooter/ImmersiveView.swift
+++ Immersive Ball Shooter/ImmersiveView.swift
@@ -4,23 +4,69 @@
 //
 //  Created by Henry Lam on 29/2/2024.
 //
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    // MARK: - Properties
+    
+    @State private var cubeList: [Entity] = []
+    @EnvironmentObject private var counter: CounterModel
+    
+    // ViewModel instance to handle business logic
+    private var model = ViewModel()
+
+    // MARK: - Body
+    
     var body: some View {
+        // RealityView to display augmented reality content
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+            // Add immersive scene to the content
+            if let scene = try? await Entity(named: "ImmersiveScene", in: realityKitContentBundle) {
                 content.add(scene)
             }
+            
+            // Add content entities and cubes
+            content.add(model.setupContentEntity())
+            
+            // Add green ball cubes based on counter.ballMax
+            for index in 0..<counter.ballMax + 20 {
+                cubeList.append(Entity())
+                cubeList[index] = model.addCube(name: "Cube\(index + 1)", greenball: "true")
+            }
+            
+            // Add additional cubes beyond counter.ballMax
+            for index in counter.ballMax..<300 {
+                cubeList.append(Entity())
+                cubeList[index] = model.addCube(name: "Cube\(index + 1)")
+            }
         }
+        // Add tap gesture to interact with entities
+        .gesture(
+            SpatialTapGesture()
+                .targetedToAnyEntity()
+                .onEnded { value in
+                    if model.removeModel(entity: value.entity) {
+                        correct()
+                    }
+                }
+        )
+    }
+
+    // MARK: - Helper Function
+    
+    // Increments the ball counter when a correct cube is removed.
+    private func correct() {
+        counter.ballCounter += 1
     }
 }
 
-#Preview {
-    ImmersiveView()
-        .previewLayout(.sizeThatFits)
+
+struct ImmersiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImmersiveView()
+            .previewLayout(.sizeThatFits)
+    }
 }
+
